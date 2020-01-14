0f8b2b56c9b89f5c6c9bc1142383dd9108eca816
AndyMcElligott <andymc2315@gmail.com>
2020-01-14 13:55:30 -0600

    finished

---
+++ src/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2> About </h2>
+                <p> we do stuff sometimes and all the time but not right now.</p>
+            </div>
+        )
+    }
+}
+
+export default About;
---
+++ src/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2> Contact</h2>
+                <p> 1-800-EpsteinDidntKillHimself </p>
+                <p> epsteindidntkillhimself@netscape.com </p>
+            </div>
+        )
+    }
+}
+
+export default Contact;
